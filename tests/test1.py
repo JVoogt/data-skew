9b1468d24a5d9079178e33b64af71e39a5f2288f
Voogt <Jean-Pierre.Voogt@experian.com>
2019-10-16 15:41:14 +0200

    Changed package from hypen to dash

---
+++ tests/test1.py
@@ -0,0 +1,15 @@
+import data_skew as ds
+import json
+import pandas as pd
+import numpy as np
+
+data = pd.DataFrame({'Gender' : ['M', 'F'] * 12,
+                   'Language' : ['P', 'F'] * 12,
+                   'Quantity' : np.random.randn(24),
+                   'Values' : np.random.random_integers(0,4,24)})
+
+with open('bias.json') as bias:
+    bias = json.load(bias)
+
+df = ds.getBiasedDataset(data,bias)
+print(df.groupby(by="Gender").size())
