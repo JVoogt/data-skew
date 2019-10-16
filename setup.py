29ec35108bbe8b7c8d3cb24ff4b6ebda81cd14b9
Voogt <Jean-Pierre.Voogt@experian.com>
2019-10-16 14:22:45 +0200

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='data-skew',
+      version='0.1',
+      description='Create DataFrames based on a required bias',
+      url='http://github.com/JVoogt/data-skew',
+      author='J Voogt',
+      author_email='jvoogt1@outlook.com',
+      license='MIT',
+      packages=['data-skew'],
+      zip_safe=False)
